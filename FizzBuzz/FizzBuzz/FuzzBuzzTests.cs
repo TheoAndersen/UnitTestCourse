cc77b338b2d00103b7e6779ecd08e157e0dc77d3
Theo Andersen <andersen.theo@gmail.com>
2012-09-24 21:27:53 +0200

    Added FizzBuzz skeleton

---
+++ FizzBuzz/FizzBuzz/FuzzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FuzzBuzzTests
+    {
+        [TestMethod]
+        public void FizzBuzz_ShouldReturnCorrectValues()
+        {
+            Assert.Fail("test");
+        }
+        
+        public string FizzBuzz(int value)
+        {
+            return ""; // Insert clever code here;
+        }
+    }
+}
