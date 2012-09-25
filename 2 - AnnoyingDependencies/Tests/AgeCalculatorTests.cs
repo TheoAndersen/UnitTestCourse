e9826a6ad5c11ac70deba73e5d3ab145a0327bc5
Theo Andersen <theo@paradis.dk>
2012-09-25 11:26:21 +0200

    added missing unit test files for AnnoyingDependencies

---
+++ 2 - AnnoyingDependencies/Tests/AgeCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using _2___AnnoyingDependencies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class AgeCalculatorTests
+    {
+        [TestMethod]
+        public void AgeCalculator_CalculatesTheRightNumberOfDays()
+        {
+            // ??
+        }
+    }
+}
