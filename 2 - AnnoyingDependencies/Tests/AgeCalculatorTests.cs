eed44077ba2601035a32597a04335e1ce56be027
Theo Andersen <theo@paradis.dk>
2012-10-01 10:14:52 +0200

    AnnoyingDependencies - Wrote the unit-test calling CalculateAgeInDays - not compiling

--- 2 - AnnoyingDependencies/Tests/AgeCalculatorTests.cs
+++ 2 - AnnoyingDependencies/Tests/AgeCalculatorTests.cs
@@ -10,7 +10,21 @@ namespace Tests
         [TestMethod]
         public void AgeCalculator_CalculatesTheRightNumberOfDays()
         {
-            // ??
+            AgeCalculator ageCalculator = new AgeCalculator();
+
+            ageCalculator.CalculateAgeInDays("31/03/1981");
         }
     }
 }
+
+/* Steps in getting it to unit test
+ * 
+ * 1. Write the unit test as should would like the code to be
+ * 
+ * 2. Refactor it so it compiles
+ * 
+ * 3. Extract the dendencies you need to stub/mock
+ * 
+ * 4. Make the test pass
+ * 
+ */
