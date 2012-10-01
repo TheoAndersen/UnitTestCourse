5ea0b506ba7310e11ee5020637a7cd8564ffdb8d
Theo Andersen <theo@paradis.dk>
2012-10-01 14:16:15 +0200

    added BowlingGameKata project

---
+++ BowlingGameKata/BowlingGameKata/BowlingGameTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameKata
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("does it work?");
+        }
+    }
+}
